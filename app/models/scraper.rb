a2801e2c91fab3a3738029e778453434ec0c3d65
Edwin <edwin.h.morris@gmail.com>
2012-07-10 20:19:43 -0400

    convert class data to a hash, print hash

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -35,23 +35,36 @@ class Scraper < ActiveRecord::Base
 
     courses_html = course_info.search('table.datadisplaytable tr')
 
-    courses_json = courses_html.map do |row|
+    courses_hash = courses_html.map do |row|
       # make sure this row is a class data row
-      #if row.children.count > 2 and row.search('> td.dddefault').count
-      {
-        :closed     => row.search('>:nth-child(1)').inner_text,
-        :crn        => row.search('>:nth-child(2)').inner_text,
-        :subject    => row.search('>:nth-child(3)').inner_text,
-        :coursenum  => row.search('>:nth-child(4)').inner_text,
-        :section    => row.search('>:nth-child(5)').inner_text,
-        :campus     => row.search('>:nth-child(6)').inner_text
-      }
-      #end
-    end.to_json
+      # check to make sure the CRN field has a CRN
+      if row.search('>:nth-child(2)').inner_text.to_i.is_a? Integer and
+         row.search('>:nth-child(2)').inner_text.to_i > 0
+        {
+          :closed     => row.search('>:nth-child(1)').inner_text,
+          :crn        => row.search('>:nth-child(2)').inner_text,
+          :subject    => row.search('>:nth-child(3)').inner_text,
+          :coursenum  => row.search('>:nth-child(4)').inner_text,
+          :section    => row.search('>:nth-child(5)').inner_text,
+          :campus     => row.search('>:nth-child(6)').inner_text,
+          :credits    => row.search('>:nth-child(7)').inner_text,
+          :title      => row.search('>:nth-child(8)').inner_text,
+          :days       => row.search('>:nth-child(9)').inner_text,
+          :time       => row.search('>:nth-child(10)').inner_text,
+          :capacity   => row.search('>:nth-child(11)').inner_text,
+          :actual     => row.search('>:nth-child(12)').inner_text,
+          :remaining  => row.search('>:nth-child(13)').inner_text,
+          :instructor => row.search('>:nth-child(14)').inner_text,
+          :date       => row.search('>:nth-child(15)').inner_text,
+          :location   => row.search('>:nth-child(16)').inner_text,
+          :attribute  => row.search('>:nth-child(17)').inner_text
+        }
+      end
+    end
 
     #courses_json = courses_html.last.search('>:nth-child(1)').inner_text
 
-    courses_json
+    courses_hash
   end
 
   # Throws courses into database
